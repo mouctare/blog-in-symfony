4fae48e727c93026a1e9f43eaf8a16cac73f5692
mouctare <mouctard78@gmail.com>
2020-04-26 12:46:07 +0200

    add modification entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,112 +2,130 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+/**
+ * @ORM\Entity
+ * @UniqueEntity(fields="email", message="Email already taken")
+ * @UniqueEntity(fields="username", message="Username already taken")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     */
+    private $username;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(max=4096)
+     */
+    private $plainPassword;
+
+    /**
+     * The below length depends on the "algorithm" you use for encoding
+     * the password, but this works well with bcrypt.
+     *
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    
+
+    // other properties and methods
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword($password)
+    {
+        $this->plainPassword = $password;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getSalt()
+    {
+        // The bcrypt and argon2i algorithms don't require a separate salt.
+        // You *may* need a real salt if you choose a different encoder.
+        return null;
+    }
 
 
         /**
-         * @ORM\Entity
-         * @UniqueEntity(fields="email", message="Email already taken")
-         * @UniqueEntity(fields="username", message="Username already taken")
-         */
-        class User implements UserInterface
-        {
-            /**
-             * @ORM\Id
-             * @ORM\Column(type="integer")
-             * @ORM\GeneratedValue(strategy="AUTO")
-             */
-            private $id;
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
 
-            /**
-             * @ORM\Column(type="string", length=255, unique=true)
-             * @Assert\NotBlank()
-             * @Assert\Email()
-             */
-            private $email;
+        return array_unique($roles);
+    }
 
-            /**
-             * @ORM\Column(type="string", length=255, unique=true)
-             * @Assert\NotBlank()
-             */
-            private $username;
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
-            /**
-             * @Assert\NotBlank()
-             * @Assert\Length(max=4096)
-             */
-            private $plainPassword;
+        return $this;
+    }
+        
+    
 
-            /**
-             * The below length depends on the "algorithm" you use for encoding
-             * the password, but this works well with bcrypt.
-             *
-             * @ORM\Column(type="string", length=64)
-             */
-            private $password;
-
-           
-
-            // other properties and methods
-
-            public function getEmail()
-            {
-                return $this->email;
-            }
-
-            public function setEmail($email)
-            {
-                $this->email = $email;
-            }
-
-            public function getUsername()
-            {
-                return $this->username;
-            }
-
-            public function setUsername($username)
-            {
-                $this->username = $username;
-            }
-
-            public function getPlainPassword()
-            {
-                return $this->plainPassword;
-            }
-
-            public function setPlainPassword($password)
-            {
-                $this->plainPassword = $password;
-            }
-
-            public function getPassword()
-            {
-                return $this->password;
-            }
-
-            public function setPassword($password)
-            {
-                $this->password = $password;
-            }
-
-            public function getSalt()
-            {
-                // The bcrypt and argon2i algorithms don't require a separate salt.
-                // You *may* need a real salt if you choose a different encoder.
-                return null;
-            }
-
-            public function getRoles()
-            {
-                return $this->roles;
-            }
-
-            public function eraseCredentials()
-            {
-            }
-        }
+    public function eraseCredentials()
+    {
+    }  
+}
