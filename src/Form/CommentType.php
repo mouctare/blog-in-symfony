1fd14c47624e1236beb7cc5c30ab399306fc28e4
mouctare <mouctard78@gmail.com>
2020-04-27 10:12:36 +0200

    addComment

---
+++ src/Form/CommentType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('author')
+            ->add('content')
+           // ->add('createdAt') on s'en ocupe nous meme
+           // ->add('article')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
